916b6f526e37d357b6747905bf4aa16682814dd6
kb4321 <kollinb321@gmail.com>
2025-01-14 03:29:58 -0800

    Updated ReportShorts

--- src/navigation.js
+++ src/navigation.js
@@ -227,6 +227,6 @@ export const footerData = {
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    NimbleNews - JB Corporations · All rights reserved.
+    ReportShorts - JB Corporations · All rights reserved.
   `,
 };
